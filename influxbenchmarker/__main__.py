191de0e8e41e0ad8dbb17844e8af5cde25a6ab3a
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-11-03 16:02:16 +0100

    initial read/write example working

--- influxbenchmarker/__main__.py
+++ influxbenchmarker/__main__.py
@@ -1,5 +1,31 @@
+# TODO: CLI PARSER
+# TODO: ADD BLACK IN CI
+
+from influxdb import InfluxDBClient
+
+HOST = ""
+PORT = 8086
+USERNAME = "grafana"
+PASSWORD = "grafana"
+DATABASE = "mydb"
+
 def main():
-    print("Hello, world!")
+    print("1")
+    json_body = [
+        {
+            "measurement": "cpu_load_short",
+            "tags": {"host": "server01", "region": "us-west"},
+            "time": "2009-11-10T23:00:00Z",
+            "fields": {"value": 1.2},
+        }
+    ]
+    client = InfluxDBClient(HOST, PORT, USERNAME, PASSWORD, DATABASE)
+    print("2")
+    client.write_points(json_body)
+    print("3")
+    result = client.query('select value from cpu_load_short;')
+    print("4")
+    print(f"{result=}")
 
 
 if __name__ == "__main__":
